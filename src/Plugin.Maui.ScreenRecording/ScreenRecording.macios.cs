29753dfada19e7fbdee43755653ba411f689e7e8
Gerald Versluis <gerald@verslu.is>
2023-03-31 20:09:05 +0200

    Add saving to disk/Photos for iOS & macOS

--- src/Plugin.Maui.ScreenRecording/ScreenRecording.macios.cs
+++ src/Plugin.Maui.ScreenRecording/ScreenRecording.macios.cs
@@ -1,4 +1,6 @@
-﻿using ReplayKit;
+﻿using Microsoft.Maui.ApplicationModel;
+using Photos;
+using ReplayKit;
 
 namespace Plugin.Maui.ScreenRecording;
 
@@ -21,11 +23,32 @@ partial class ScreenRecordingImplementation : IScreenRecording
 	{
 		if (RPScreenRecorder.SharedRecorder.Recording)
 		{
-			await RPScreenRecorder.SharedRecorder.StopRecordingAsync();
+			var tempPath = NSUrl.FromFilename(Path.Combine(Path.GetTempPath(),
+				$"screenrecording-{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")}.mp4"));
 
-			// TODO save to disk
-			// TODO save to gallery?
+			await RPScreenRecorder.SharedRecorder.StopRecordingAsync(tempPath);
+
+			var permissionResult = await Permissions.RequestAsync<Permissions.PhotosAddOnly>();
+
+			if (permissionResult != PermissionStatus.Granted)
+			{
+				// TODO what do we do here?
+				return;
+			}
+
+			PHPhotoLibrary.SharedPhotoLibrary.PerformChanges(() =>
+			{
+				PHAssetChangeRequest.FromVideo(tempPath);
+			}, (success, error) =>
+			{
+				// TODO what to do here?
+			});
+
 			// TODO show preview view controller?
+			// Show preview after recording, only on iOS/macOS
+			//var cv = await RPScreenRecorder.SharedRecorder.StopRecordingAsync();
+
+			//Platform.GetCurrentUIViewController().ShowViewController(cv, cv);
 		}
 	}
 }
